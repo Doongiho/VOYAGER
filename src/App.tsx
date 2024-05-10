519d7c5a6241f789b311650273c0fe2459d5ec40
seojiho <happyluck0426@gamil.com>
2024-05-10 21:17:00 +0900

    DESIGN: footer 페이지 레이아웃 생성 및 css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header';
 import Main from './page/main/main'
+import Footer from './components/footer';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
