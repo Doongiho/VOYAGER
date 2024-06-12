3dfdf644caee5d8bcf27a66cb8a4be88bab48284
seojiho <happyluck0426@gamil.com>
2024-06-12 14:09:24 +0900

    feat: 동영상 상세페이지에 내용 불러오기 구현

--- src/page/videoSales/videoManagement.tsx
+++ src/page/videoSales/videoManagement.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import ImageUpload from '../../assests/Upload.png';
+import { IFormInput } from '../../types/IFormInput';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
+const VideoManagement: React.FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const video = location.state?.video as IFormInput;
 
-const videoManagement: React.FC = () => {
+  const { register, handleSubmit } = useForm<IFormInput>({
+    defaultValues: {
+      title: video.title,
+      explanation: video.explanation,
+      price: video.price,
+    },
+  });
+
+  if (!video) {
+    return <div>비디오 데이터를 찾을 수 없습니다.</div>;
+  }
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    const updatedVideo = { ...video, ...data };
+    navigate('/videoSales', { state: { updatedVideo } });
+  };
+
   return (
     <VideoContainer>
       <VideoDiv>
@@ -15,7 +37,9 @@ const videoManagement: React.FC = () => {
                   <Videoh1>동영상 관리</Videoh1>
                 </ExplanationLi1>
                 <ExplanationLi>
-                  <DivVideo1></DivVideo1>
+                  <DivVideo1>
+                    {video.videoFile && <VideoThumbnail src={URL.createObjectURL(video.videoFile)} />}
+                  </DivVideo1>
                   <ExplanationP1>구매수:</ExplanationP1>
                 </ExplanationLi>
               </ExplanationUl>
@@ -23,21 +47,23 @@ const videoManagement: React.FC = () => {
           </PurchaseDiv>
           <ExplanationDiv>
             <ExplanationUl>
-              <ExplanationLi>
-                <ExplanationP>제목</ExplanationP>
-                <Input />
-              </ExplanationLi>
-              <ExplanationLi>
-                <ExplanationP>설명</ExplanationP>
-                <Input />
-              </ExplanationLi>
-              <ExplanationLi>
-                <ExplanationP>판매가격</ExplanationP>
-                <Input />
-              </ExplanationLi>
-              <ExplanationLi>
-                <ExplanationButton>저장하기</ExplanationButton>
-              </ExplanationLi>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <ExplanationLi>
+                  <ExplanationP>제목</ExplanationP>
+                  <Input {...register('title')} />
+                </ExplanationLi>
+                <ExplanationLi>
+                  <ExplanationP>설명</ExplanationP>
+                  <Input {...register('explanation')} />
+                </ExplanationLi>
+                <ExplanationLi>
+                  <ExplanationP>판매가격</ExplanationP>
+                  <Input {...register('price')} />
+                </ExplanationLi>
+                <ExplanationLi>
+                  <ExplanationButton type="submit">저장하기</ExplanationButton>
+                </ExplanationLi>
+              </form>
             </ExplanationUl>
           </ExplanationDiv>
         </Div>
@@ -45,78 +71,81 @@ const videoManagement: React.FC = () => {
     </VideoContainer>
   );
 };
-
 const VideoDiv = styled.div`
   flex-direction: column;
-  width:80%;
+  width: 80%;
   margin: 0 auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  
 `;
+const VideoThumbnail = styled.video`
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+`;
+
 const VideoContainer = styled.div`
   background-color: #202124;
   padding: 100px 0;
 `;
 
 const Videoh1 = styled.h1`
-    text-align: left;
-    font-size: 25px;
-    font-weight: bolder;
-}
+  text-align: left;
+  font-size: 25px;
+  font-weight: bolder;
 `;
 
 const Div = styled.div`
-  width:100%;
-  display:flex;
+  width: 100%;
+  display: flex;
   justify-content: space-between;
   height: 100%;
-  margin-top:50px;
+  margin-top: 50px;
 `;
 
 const ExplanationButton = styled.button`
-    border-radius: 1rem;
-    border: 1px solid #907AE7;
-    background: #907AE7;
-    cursor: pointer;
-    padding: 12px;
-    color: #fff;
-    box-shadow: 2px 2px 2px #b2b2b2;
-    font-size: 17px;
-    font-weight: bolder;
-    margin-top: 70px;
-}
-    
+  border-radius: 1rem;
+  border: 1px solid #907AE7;
+  background: #907AE7;
+  cursor: pointer;
+  padding: 12px;
+  color: #fff;
+  box-shadow: 2px 2px 2px #b2b2b2;
+  font-size: 17px;
+  font-weight: bolder;
+  margin-top: 70px;
 `;
+
 const Input = styled.input`
-    border: 1px solid #00000012;
-    background: #00000012;
-    color: #3b393973;
-    font-weight: bolder;
-    cursor: pointer;
-    box-shadow: 2px 2px 2px #b2b2b2;
-    width: 100%;
-    padding: 0.6rem 0rem 0.6rem 0rem;
-    line-height: 24px;
-    border-radius: 1rem;
-    font-size:17px;
-    &:focus {
-        outline: none;
-        border-color: #3b393973;
-    }
+  border: 1px solid #00000012;
+  background: #00000012;
+  color: #3b393973;
+  font-weight: bolder;
+  cursor: pointer;
+  box-shadow: 2px 2px 2px #b2b2b2;
+  width: 100%;
+  padding: 0.6rem 0rem 0.6rem 0rem;
+  line-height: 24px;
+  border-radius: 1rem;
+  font-size: 17px;
+  &:focus {
+    outline: none;
+    border-color: #3b393973;
+  }
 `;
 
 const PurchaseDiv = styled.div`
- width:45%;
-
+  width: 45%;
 `;
+
 const ExplanationDiv = styled.div`
-  width:45%;
+  width: 45%;
   border: 1px solid #00000012;
   background: #00000012;
   font-weight: bolder;
-  box-shadow: 2px 2px 2px #b2b2b2;
+  box-shadow: 2px 
+ 2px 2px #b2b2b2;
   line-height: 24px;
   border-radius: 1rem;
   background:#fff;
@@ -182,4 +211,4 @@ const ExplanationP1 = styled.p`
     padding: 0px;
     margin-top: 20px;
 `;
-export default videoManagement;
+export default VideoManagement;
