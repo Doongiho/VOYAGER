a8f2c64ca54f40fc14fdc053ab985d59e50100d6
seojiho <happyluck0426@gamil.com>
2024-06-15 15:31:14 +0900

    refactor: VideoManager 컴포넌트에서 'videos' 상태를 사용하도록 리팩토링

--- src/page/videoSales/VideoManager.tsx
+++ src/page/videoSales/VideoManager.tsx
@@ -5,22 +5,21 @@ import VideoManagement from './videoSales';
 import { IVideo } from '../../types/IVideo';
 
 const VideoManager: React.FC = () => {
-    const [videoSales, setVideoSales] = useState<IVideo[]>([]);
+    const [videos, setVideos] = useState<IVideo[]>([]);
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
     const handleAddVideo = (newVideo: IVideo) => {
-        setVideoSales([...videoSales, newVideo]);
+        setVideos([...videos, newVideo]);
     };
     const handleDelete = (videoId: string) => {
-        const updatedVideoSales = videoSales.filter(video => video.id !== videoId);
-        setVideoSales(updatedVideoSales);
+        const updatedVideoSales = videos.filter(video => video.id !== videoId);
+        setVideos(updatedVideoSales);
     };
-
 
     return (
         <Routes>
             <Route path="/videoUpload" element={<VideoUpload onAddVideo={handleAddVideo} />} />
-            <Route path="/videoManagement" element={<VideoManagement videoSales={videoSales} isLoggedIn={isLoggedIn} onDeleteVideo={handleDelete} />} />
+            <Route path="/videoManagement" element={<VideoManagement videos={videos} isLoggedIn={isLoggedIn} onDeleteVideo={handleDelete} />} />
         </Routes>
     );
 };
