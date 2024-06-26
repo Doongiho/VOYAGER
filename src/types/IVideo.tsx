fbe54fec439b6d875436e95d50dbf6cf10c4446b
seojiho <happyluck0426@gmail.com>
2024-06-26 13:52:41 +0900

    refactor: IVideo 인터페이스 업데이트

--- src/types/IVideo.tsx
+++ src/types/IVideo.tsx
@@ -3,9 +3,12 @@ export interface IVideo {
     title: string;
     explanation: string;
     price: number;
+    isValid: boolean;
     isLoggedIn: boolean;
     id: string;
     videoStr: string;
     username: string;
+    useremail:string;
     twitterImage: string;
+    videoUrl: string;
 }
